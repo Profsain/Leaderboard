f7020aa8f0b8f99f63c3fb12cd6afaec7ec0f60a
Profsain <profsaincrown@gmail.com>
2022-06-16 16:48:27 +0100

    Add CSS styling

--- src/modules/fetchscoredata.js
+++ src/modules/fetchscoredata.js
@@ -1,6 +1,11 @@
 const fetchScoreData = async () => {
   let url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/:id/scores';
+  const indicator = document.querySelector('.indicator');
+
   try {
+
+    indicator.textContent = '........loading';
+    indicator.style.display = 'block';
     let response = await fetch(url);
     return await response.json();
 
